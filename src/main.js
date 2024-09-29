268e0cb42db9756a528546c7020a9f0845a75c8c
Kasia24 <160325853+Kasia24@users.noreply.github.com>
2024-09-29 10:28:29 +0200

    add commit

--- src/main.js
+++ src/main.js
@@ -1,5 +1,25 @@
-// Twój unikalny klucz API Pixabay
+import axios from 'axios';
+
 const API_KEY = '46058905-76d6ace161caaf887286baf22';
+const BASE_URL = 'https://pixabay.com/api/';
+
+async function fetchImages(query, page = 1) {
+  try {
+    const response = await axios.get(BASE_URL, {
+      params: {
+        key: API_KEY,
+        q: query,
+        image_type: 'photo',
+        per_page: 40,
+        page: page,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching images:', error);
+    throw error;
+  }
+}
 
 // Elementy DOM
 const searchForm = document.getElementById('search-form');
@@ -9,12 +29,6 @@ const loader = document.getElementById('loader');
 
 // Inicjalizacja SimpleLightbox
 let lightbox = new SimpleLightbox('.gallery a');
-
-// Funkcja wyszukiwania obrazów
-const searchImages = async query => {
-  const url = `https://pixabay.com/api/?key=${API_KEY}&q=${encodeURIComponent(
-    query
-  )}&image_type=photo&orientation=horizontal&safesearch=true`;
 
   // Funkcja pokazująca i ukrywająca loader
   function showLoader() {
